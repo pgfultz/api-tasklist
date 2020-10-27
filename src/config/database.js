400d03e0c8c8ce79de491680a3032e7bb4ea1d4b
Lucas Morais <lucas.s.morais23@gmail.com>
2020-10-26 23:10:02 -0300

    Config postgres and model User

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '34.95.137.251',
+  username: 'postgres',
+  password: 'lucas123',
+  database: 'tasklist',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
