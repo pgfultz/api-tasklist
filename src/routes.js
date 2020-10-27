2da0d99afcf1f38b5d45b54c70b3d07dd879124f
Lucas Morais <lucas.s.morais23@gmail.com>
2020-10-26 23:24:22 -0300

    Loading models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello world!!!' });
+});
+
+routes.get('/teste', async (req, res) => {
+  const user = await User.create({
+    name: 'Lucas',
+    email: 'lucasdasilva165@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
