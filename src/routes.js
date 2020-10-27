903711aac4116aaddcce7ebe20fd110775c99be8
Lucas Morais <lucas.s.morais23@gmail.com>
2020-10-26 23:45:11 -0300

    Cadastrando usuario e gerando hash de senha

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
@@ -7,14 +8,6 @@ routes.get('/', (req, res) => {
   return res.json({ message: 'Hello world!!!' });
 });
 
-routes.get('/teste', async (req, res) => {
-  const user = await User.create({
-    name: 'Lucas',
-    email: 'lucasdasilva165@gmail.com',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.post('/user', UserController.store);
 
 export default routes;
